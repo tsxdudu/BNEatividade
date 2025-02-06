0ac59da5fed987afa22880e2b9687c8835777227
☆ <141635170+tsxdudu@users.noreply.github.com>
2025-02-06 10:33:19 -0300

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Title.WriteTitle();
+        while (true)
+        {
+            Console.WriteLine("·············································································");
+            Console.WriteLine("Escolha qual você deseja executar:" +
+            "\n- 1) Primeiro Teste de Mesa (atividade 1)" +
+            "\n- 2) Segundo Teste de Mesa (atividade 1)" +
+            "\n- 3) Terceiro Teste de Mesa (atividade 1)");
+            Console.Write(":");
+            int numberIndex = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("·············································································");
+            switch (numberIndex)
+            {
+                case 1:
+                    TesteDeMesa.Part1();
+                    break;
+                case 2:
+                    TesteDeMesa.Part2();
+                    break;
+                case 3:
+                    TesteDeMesa.Part3();
+                    break;
+                case 4:
+                default:
+                    Console.WriteLine("ERRO: O programa não foi encontrado, tente outro.");
+                    break;
+            }
+        }
+    }
+}
